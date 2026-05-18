67a7ddad830bb3f65b28b8b19f177c69b53eefec
masha <126472968+mariumi-ma@users.noreply.github.com>
2026-05-18 10:28:23 +0300

    добавлены теги omitempty в параметры отчёта

--- report.go
+++ report.go
@@ -13,23 +13,23 @@ type Label struct {
 }
 
 type ReportParam struct {
-	ManufactureDate          string `json:"manufacture_date"`           // (25) Дата изготовления (производства) товара
-	ItemName                 string `json:"item_name"`                  // (94) Наименование товара
-	MarkMethod               string `json:"mark_method"`                // (95) Способ маркировки (полиграфическая защита)
-	DeclarationRegNumber     string `json:"declaration_reg_number"`     // (96) Регистрационный номер декларации на товары
-	DeclarationReleaseDate   string `json:"declaration_release_date"`   // (97) Дата выпуска товаров по декларации
-	Country                  string `json:"country"`                    // (100) Страна экспорта
-	MarkTarget               string `json:"mark_target"`                // (101) Цель маркировки
-	ContractDate             string `json:"dogovor_date"`               // (102) Основание: дата документа
-	ContractNumber           string `json:"dogovor_nomer"`              // (103) Основание: номер документа
-	MarkReason               string `json:"mark_reason"`                // (104) Причина нанесения СИ
-	DocumentDate             string `json:"document_date"`              // (109) Дата документа, подтверждающего приобретение товара
-	DocumentNumber           string `json:"document_number"`            // (110) Номер документа, подтверждающего приобретение товара
-	TaxPayerNumber           string `json:"org_number"`                 // (111) Номер налогоплательщика
-	SellerName               string `json:"org_name"`                   // (112) Наименование организации-продавца
-	RemarkReason             string `json:"remark_reason"`              // (124) Причина перемаркировки
-	ExportCountryEAEU        string `json:"export_country_eaeu"`        // (126) Страна экспорта (ЕАЭС)
-	DeclarationOrdinalNumber string `json:"declaration_ordinal_number"` // (134) Порядковый номер товара в декларации на товары
+	ManufactureDate          string `json:"manufacture_date,omitempty"`           // (25) Дата изготовления (производства) товара
+	ItemName                 string `json:"item_name,omitempty"`                  // (94) Наименование товара
+	MarkMethod               string `json:"mark_method,omitempty"`                // (95) Способ маркировки (полиграфическая защита)
+	DeclarationRegNumber     string `json:"declaration_reg_number,omitempty"`     // (96) Регистрационный номер декларации на товары
+	DeclarationReleaseDate   string `json:"declaration_release_date,omitempty"`   // (97) Дата выпуска товаров по декларации
+	Country                  string `json:"country,omitempty"`                    // (100) Страна экспорта
+	MarkTarget               string `json:"mark_target,omitempty"`                // (101) Цель маркировки
+	ContractDate             string `json:"dogovor_date,omitempty"`               // (102) Основание: дата документа
+	ContractNumber           string `json:"dogovor_nomer,omitempty"`              // (103) Основание: номер документа
+	MarkReason               string `json:"mark_reason,omitempty"`                // (104) Причина нанесения СИ
+	DocumentDate             string `json:"document_date,omitempty"`              // (109) Дата документа, подтверждающего приобретение товара
+	DocumentNumber           string `json:"document_number,omitempty"`            // (110) Номер документа, подтверждающего приобретение товара
+	TaxPayerNumber           string `json:"org_number,omitempty"`                 // (111) Номер налогоплательщика
+	SellerName               string `json:"org_name,omitempty"`                   // (112) Наименование организации-продавца
+	RemarkReason             string `json:"remark_reason,omitempty"`              // (124) Причина перемаркировки
+	ExportCountryEAEU        string `json:"export_country_eaeu,omitempty"`        // (126) Страна экспорта (ЕАЭС)
+	DeclarationOrdinalNumber string `json:"declaration_ordinal_number,omitempty"` // (134) Порядковый номер товара в декларации на товары
 }
 
 type ReportMarkResponse struct {
